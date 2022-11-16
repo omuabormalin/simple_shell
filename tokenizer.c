b78279426536e01fcc2489878eed0902eef38a53
Daniel-Uchay <meetcaxtroh@gmail.com>
2022-11-16 19:26:46 +0100

    last edit

--- tokenizer.c
+++ tokenizer.c
@@ -3,7 +3,7 @@
 /**
  * **strtow - splits a string into words. Repeat delimiters are ignored
  * @str: the input string
- * @d: the delimeter string
+ * @d: the delimeter stringi.
  * Return: a pointer to an array of strings, or NULL on failure
  */
 
